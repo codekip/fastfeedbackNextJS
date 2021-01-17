1cd9d5133143435bea2e6ced8b5b3933301937f4
Nick Sawe <codekip@outlook.com>
2021-01-17 20:55:15 +0000

    hello two

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My fast feedback site!
+          My fast feedback site
         </h1>
 
         <p className={styles.description}>
